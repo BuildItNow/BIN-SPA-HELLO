4602762b41d9d2c686f5b8dceaa2cd73f587ad6c
Yang G <pinner@mail.ustc.edu.cn>
2017-11-26 00:26:53 +0800

    1: Optimize css for desktop browser when display size is too big.

--- client/application/application.js
+++ client/application/application.js
@@ -1,5 +1,5 @@
 define(
-["bin/core/spaApplication", "application/dataCenter", "css!application/css/application.css"],
+["bin/core/spaApplication", "application/dataCenter"],
 function(Base, DataCenter)
 {
 	var Application = {};
@@ -9,6 +9,26 @@ function(Base, DataCenter)
 		Base.prototype.run.call(this);
 		
 		bin.naviController.startWith("home/index");
+	}
+
+	Application.onResize = function()
+	{
+		Base.prototype.onResize.call(this);
+
+		var w = this._width;
+		if(w <= 420)
+		{
+			return ;
+		}
+		this._width = 420;
+
+		var elemRoot = document.documentElement;
+		if(!this._fontSize)
+		{
+			var f = parseInt(elemRoot.style.fontSize);
+			this._fontSize = parseInt(420*f/w)+"px";
+		}
+		elemRoot.style.fontSize = this._fontSize;
 	}
 
 	return Base.extend(Application);
